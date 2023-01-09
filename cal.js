dcc3be3e4be0c07898bd8ba92e648c35080b8e24
Avinash Chourasia <97829396+AvinashChourasia@users.noreply.github.com>
2023-01-09 22:32:55 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,36 @@
+let string = "";
+
+let buttons = document.querySelectorAll('.b1');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'A'){
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'x'){
+            string = string.slice(0, -1);
+            document.querySelector('input').value = string;
+        }
+        else {
+            console.log(e.target);
+            if(e.target.innerHTML == '÷'){
+                string += '/';
+            }
+            else if(e.target.innerHTML == '^'){
+                string += '**';
+            }
+            else{
+                string = string + e.target.innerHTML;
+            }
+            document.querySelector('input').value = string;
+        }
+    })
+
+});
+
+
+// prompt("What !")
